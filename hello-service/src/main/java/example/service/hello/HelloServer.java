529bc5ab36dbf6d85082e5a2f7201161bbceb428
gregwhitaker <gwhitake@gmail.com>
2019-11-05 15:45:13 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/HelloServer.java
+++ hello-service/src/main/java/example/service/hello/HelloServer.java
@@ -1,5 +1,8 @@
 package example.service.hello;
 
+import io.servicetalk.http.netty.HttpServers;
+import io.servicetalk.http.router.jersey.HttpJerseyRouterBuilder;
+import io.servicetalk.transport.api.ServerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +16,13 @@ public class HelloServer {
         // Noop
     }
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        ServerContext serverContext = HttpServers.forPort(8080)
+                .listenAndAwait(new HttpJerseyRouterBuilder()
+                        .build(new HelloServiceApplication()));
 
+        LOG.info("Listening on {}", serverContext.listenAddress());
+
+        serverContext.awaitShutdown();
     }
 }
