10800326e61eb162189727446b510a664ee17326
gregwhitaker <gwhitake@gmail.com>
2019-11-06 07:53:28 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/HelloResource.java
+++ hello-service/src/main/java/example/service/hello/HelloResource.java
@@ -1,6 +1,8 @@
 package example.service.hello;
 
 import example.service.hello.model.HelloResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -11,11 +13,13 @@ import javax.ws.rs.core.MediaType;
 
 @Path("v1")
 public class HelloResource {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloResource.class);
 
     @GET
     @Path("hello")
     @Produces(MediaType.APPLICATION_JSON)
     public HelloResponse sayHello(@QueryParam("name") @DefaultValue("You") String name) {
+        LOG.info("Received hello request for: {}", name);
         return new HelloResponse(String.format("Hello, %s!", name));
     }
 }
