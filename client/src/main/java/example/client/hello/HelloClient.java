4a406abf9932be5b05712274be634474cd0deaf5
gregwhitaker <gwhitake@gmail.com>
2019-11-06 06:59:15 -0800

    Doing work

--- client/src/main/java/example/client/hello/HelloClient.java
+++ client/src/main/java/example/client/hello/HelloClient.java
@@ -1,6 +1,13 @@
 package example.client.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Client that sends requests to the hello-service application.
+ */
 public class HelloClient {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloClient.class);
 
     public static void main(String... args) {
 
