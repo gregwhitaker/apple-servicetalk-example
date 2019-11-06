a9ab37af6d5f952933f0d4a801aa5b718118f07b
gregwhitaker <gwhitake@gmail.com>
2019-11-06 07:15:24 -0800

    Doing work

--- client/src/main/java/example/client/hello/HelloClient.java
+++ client/src/main/java/example/client/hello/HelloClient.java
@@ -1,7 +1,16 @@
 package example.client.hello;
 
+import io.servicetalk.data.jackson.JacksonSerializationProvider;
+import io.servicetalk.http.api.HttpClient;
+import io.servicetalk.http.api.HttpDeserializer;
+import io.servicetalk.http.api.HttpSerializationProvider;
+import io.servicetalk.http.netty.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+import static io.servicetalk.http.api.HttpSerializationProviders.jsonSerializer;
 
 /**
  * Client that sends requests to the hello-service application.
@@ -9,7 +18,31 @@ import org.slf4j.LoggerFactory;
 public class HelloClient {
     private static final Logger LOG = LoggerFactory.getLogger(HelloClient.class);
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        String name = null;
+        if (args.length == 1) {
+            name = args[0];
+        }
 
+        CountDownLatch latch = new CountDownLatch(1);
+
+        LOG.info("Sending request to hello service for name: '{}'", name);
+
+        HttpSerializationProvider serializer = jsonSerializer(new JacksonSerializationProvider());
+
+        try (HttpClient client = HttpClients.forSingleAddress("localhost", 8080).build()) {
+            String url = "/v1/hello";
+            if (name != null && !name.isEmpty()) {
+                url += "?name=" + name;
+            }
+
+            client.request(client.get(url))
+                    .whenFinally(latch::countDown)
+                    .subscribe(httpResponse -> {
+                        LOG.info("Response: " + httpResponse.payloadBody(serializer.deserializerFor(HelloResponse.class)));
+                    });
+        }
+
+        latch.await();
     }
 }
---
+++ client/src/main/java/example/client/hello/HelloResponse.java
@@ -0,0 +1,21 @@
+package example.client.hello;
+
+public class HelloResponse {
+
+    private String message;
+
+    public HelloResponse() {}
+
+    public HelloResponse(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
