72405bf77252852b55782afab05078f2c9c58b4e
gregwhitaker <gwhitake@gmail.com>
2019-11-06 06:57:17 -0800

    Doing work

---
+++ client/src/main/java/example/client/hello/HelloClient.java
@@ -0,0 +1,8 @@
+package example.client.hello;
+
+public class HelloClient {
+
+    public static void main(String... args) {
+
+    }
+}
--- hello-service/src/main/java/example/service/hello/HelloResource.java
+++ hello-service/src/main/java/example/service/hello/HelloResource.java
@@ -2,9 +2,11 @@ package example.service.hello;
 
 import example.service.hello.model.HelloResponse;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("v1")
@@ -13,7 +15,7 @@ public class HelloResource {
     @GET
     @Path("hello")
     @Produces(MediaType.APPLICATION_JSON)
-    public HelloResponse sayHello() {
-        return new HelloResponse("Hello, You!");
+    public HelloResponse sayHello(@QueryParam("name") @DefaultValue("You") String name) {
+        return new HelloResponse(String.format("Hello, %s!", name));
     }
 }
